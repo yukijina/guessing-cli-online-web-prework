b501b81ccd8e632870b42e6081351db38ee4251b
Yuki N <lunarbase@outlook.com>
2018-11-15 03:15:04 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
     puts 'Goodbye!'
   elsif user_input.to_i == number
     puts "You guessed the correct number!"
-    return "exit"
+    break
   elsif user_input != number 
     puts "The computer guessed #{number}."
   end
